04262807427ff542c9b226b9740d4623c0f21568
Simon Sterckx <simon.sterckx@student.ucll.be>
2023-06-18 23:39:17 +0200

    "initial commit"

---
+++ src/main/java/net/clutchsquad/clutchsquadmod/ClutchSquadMod.java
@@ -0,0 +1,52 @@
+package net.clutchsquad.clutchsquadmod;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ClutchSquadMod.MOD_ID)
+public class ClutchSquadMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "clutchsquadmod";
+    // Directly reference a slf4j logger
+
+    public ClutchSquadMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
